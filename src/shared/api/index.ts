060d67c70d2ed88f8c58edb80c0546f12c2fcf6d
Elyas Abdullah <elyasabdulla771124@gmail.com>
2025-01-08 19:56:18 +0300

    Created axios instance

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,1 +1,10 @@
-export * from "./auth";
+import axios from "axios";
+export * from "./";
+
+export const apiClient = axios.create({
+  baseURL: process.env.SERVER_URI || "http://127.0.0.1:8000",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
