2cd9fd9b1fe8307c885b4411df7f892d4b9a6acd
Elyas Abdullah <99258943+elyasabdullah@users.noreply.github.com>
2025-03-21 09:26:48 +0300

    completed showing profile data

--- src/app/profile/Profile.hook.ts
+++ src/app/profile/Profile.hook.ts
@@ -1,3 +1,35 @@
+import { UserContext } from "@/shared/common/authentication";
+import { SERVER_URI } from "@/shared/utils";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useContext } from "react";
+
 export const useProfileHook = () => {
-  return "good";
+  const { token } = useContext(UserContext);
+  // const { id } = user;
+  const fetchProfileData = async () => {
+    const response = await axios.get(`${SERVER_URI}/api/customer/profile`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  };
+
+  const {
+    isPending: isLoading,
+    isError,
+    isSuccess,
+    data: profileData,
+  } = useQuery({
+    queryKey: ["profileData"],
+    queryFn: fetchProfileData,
+    enabled: !!token,
+  });
+  return {
+    isLoading,
+    isError,
+    isSuccess,
+    profileData: profileData?.data,
+  };
 };
