6723fbe7d073d54d45d1167e63008c7a1890a90b
Elyas Abdullah <99258943+elyasabdullah@users.noreply.github.com>
2025-04-12 09:52:49 +0300

    feat: modify cart product quantity

--- src/app/cart/components/FullPage/CartProductCard/useCartProductCard.hook.ts
+++ src/app/cart/components/FullPage/CartProductCard/useCartProductCard.hook.ts
@@ -2,10 +2,12 @@ import { UserContext } from "@/shared/common/authentication";
 import { SERVER_URI } from "@/shared/utils";
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 export const useProductCardHook = ({ id }) => {
   const { token } = useContext(UserContext);
+  const [openEditQuantityDialog, setOpenEditQuantityDialog] = useState(false);
+  const [quantity, setQuantity] = useState<string>("");
 
   const {
     mutate,
@@ -16,6 +18,24 @@ export const useProductCardHook = ({ id }) => {
       return axios.post(
         `${SERVER_URI}/api/wishlists/products/${id}`,
         {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+    },
+  });
+
+  const {
+    mutate: modifyQuantity,
+    isError: isErrorModifyingQuantity,
+    isSuccess: isSuccessModifyingQuantity,
+  } = useMutation({
+    mutationFn: (quantityInput: { quantity: string }) => {
+      return axios.patch(
+        `${SERVER_URI}/api/carts/products/${id}`,
+        quantityInput,
         {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -49,13 +69,31 @@ export const useProductCardHook = ({ id }) => {
     deleteProduct();
   };
 
+  const handleOpenQuantityDialog = () => {
+    setOpenEditQuantityDialog(!openEditQuantityDialog);
+  };
+
+  const handleChangeQuantity = (e) => {
+    setQuantity(e.target.value);
+  };
+
+  const handleSendNewQuantity = () => {
+    modifyQuantity({ quantity });
+  };
+
   return {
     isSuccessDeletingProduct,
     isErrorDeletingProduct,
     isErrorAddToWishlist,
     isSuccessAddToWishlist,
+    isErrorModifyingQuantity,
+    isSuccessModifyingQuantity,
+    openEditQuantityDialog,
     handleAddToFavorite,
     handleRemoveFromCart,
     handleCategoryProduct,
+    handleOpenQuantityDialog,
+    handleSendNewQuantity,
+    handleChangeQuantity,
   };
 };
