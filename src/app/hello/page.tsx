4191ec52d724994349b8508e5abd0937cadc1cf5
Elyas Abdullah <99258943+elyasabdullah@users.noreply.github.com>
2025-04-21 14:04:31 +0300

    nothing

---
+++ src/app/hello/page.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+
+export default function Hello() {
+  return (
+    <Box
+      display="grid"
+      gridTemplateColumns="repeat(auto-fill, minmax(300px, 1fr))"
+      gap={2}
+      padding={3}
+    >
+      Testing
+    </Box>
+  );
+}
